d02cec945aa6d806954f873c2fb854781493e8e7
Blevis <blevis.allushi@gmail.com>
2026-05-15 18:35:21 +0200

    Created base folder structure and environment. If you're a team member reading this. Don't do what I just did (committing directly to main :P)

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("===================================")
+    print(" ROUTEFORGE")
+    print(" Graph Delivery Optimization System")
+    print("===================================")
+
+if __name__ == "__main__":
+    main()
